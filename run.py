242dc63cba14de6d3ecf534f114c89add835ff7f
Ivan Toulou <touloumbadjiani@gmail.com>
2018-08-20 15:38:16 +0000

    created a game over page where yes button restarts game and exit button redirects to index.html

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ from flask import Flask, redirect, render_template, request
 app = Flask(__name__)
 
 riddle_index = 0
+counter = 0
 
 def write_to_file(filename, data):
     """
@@ -18,28 +19,12 @@ def save_user(username):
     Add usernames to the `usernames.txt` list
     """
     write_to_file("data/usernames.txt", "{0}\n".format(username))
-    
-def add_answers(username, message):
-    """
-    Add messages to the `messages` list
-    """
-    write_to_file("data/users_answer.txt", "({0} - {1}\n".format(
-           username.title(),
-           message))
-    
-def get_all_answers():
-    """
-    Get all the messages and separate them by a `br`
-    """
-    messages = []
-    with open("data/users_answer.txt", "r") as chat_messages:
-        messages = chat_messages.readlines()
-    return messages     
+
 
 @app.route('/', methods=["GET", "POST"])
 def index():
     """
-    Handle POST
+    send username to usernames.txt file and send to user's homepage
     """
     if request.method == "POST":
         write_to_file("data/usernames.txt", request.form["username"] + "\n")
@@ -50,7 +35,7 @@ def index():
 
 @app.route('/<username>', methods = ["GET", "POST"])
 def user(username):
-    global riddle_index
+    global riddle_index, counter
     data = []
     
     with open("data/riddles.json", "r") as json_data:
@@ -58,36 +43,49 @@ def user(username):
         
       
     if request.method == "POST" and "check" in request.form:
-        #Correct answer 
+        #Click check button to see if answer is correct
         if request.form["answer"].lower() == data[riddle_index]["answer"]:
             
-            
+            counter += 1
             return render_template("riddles.html", username = username, data = data[riddle_index]["question"], correct = "Correct!")
             
         else:
-            
+           #if answer is incorrect 
             return render_template("riddles.html", username = username, data = data[riddle_index]["question"], correct = "Wrong answer, try again")
             
     elif request.method == "POST" and "next" in request.form:
-        riddle_index += 1
-        #if len(data) == riddle_index:
-            #move to new a page
+        #to next riddle
         
+        riddle_index += 1
+        
+        if len(data) == riddle_index:
+            #move to new a page if game is finished
+            riddle_index = 0
+            return render_template("exitgame.html", mark = counter)
+            
+            
+                
         return render_template("riddles.html", username = username, data = data[riddle_index]["question"])
     
     elif request.method == "POST" and "previous" in request.form:
+        #to previous riddle
         riddle_index -= 1
         if len(data) < 0:
             riddle_index = 0
         return render_template("riddles.html", username = username, data = data[riddle_index]["question"])
 
+    if request.method == "POST" and "exit-game" in request.form:
+                return render_template("index.html") 
     
+    if request.method == "POST" and "leaderboard" in request.form:
+                return redirect(/'leaderboard')             
     
     return render_template("riddles.html", username = username, data = data[riddle_index]["question"])
     
     
+@app.route('/leaderboard', methods = ["GET"])
+def show_leaderboard():
     
-
 
   
 app.run(os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
