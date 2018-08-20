af8c22d0253fcf45f6fcc88293520818e84d4f97
Ivan Toulou <touloumbadjiani@gmail.com>
2018-08-20 14:25:21 +0000

    New check button added to check correct or incorrect answers along with previous button to move to a previous question

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 import os
-from datetime import datetime
+import json
 from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
+
+riddle_index = 0
 
 def write_to_file(filename, data):
     """
@@ -16,6 +18,23 @@ def save_user(username):
     Add usernames to the `usernames.txt` list
     """
     write_to_file("data/usernames.txt", "{0}\n".format(username))
+    
+def add_answers(username, message):
+    """
+    Add messages to the `messages` list
+    """
+    write_to_file("data/users_answer.txt", "({0} - {1}\n".format(
+           username.title(),
+           message))
+    
+def get_all_answers():
+    """
+    Get all the messages and separate them by a `br`
+    """
+    messages = []
+    with open("data/users_answer.txt", "r") as chat_messages:
+        messages = chat_messages.readlines()
+    return messages     
 
 @app.route('/', methods=["GET", "POST"])
 def index():
@@ -24,18 +43,51 @@ def index():
     """
     if request.method == "POST":
         write_to_file("data/usernames.txt", request.form["username"] + "\n")
-        print(request.form)
+        
         return redirect(request.form["username"]) 
     
     return render_template("index.html")    
 
-@app.route('/<username>')
+@app.route('/<username>', methods = ["GET", "POST"])
 def user(username):
-    """
-    changes to individual user's homepage
-    """
-    return render_template("riddles.html", username = username)
+    global riddle_index
+    data = []
+    
+    with open("data/riddles.json", "r") as json_data:
+        data = json.load(json_data)
+        
+      
+    if request.method == "POST" and "check" in request.form:
+        #Correct answer 
+        if request.form["answer"].lower() == data[riddle_index]["answer"]:
+            
+            
+            return render_template("riddles.html", username = username, data = data[riddle_index]["question"], correct = "Correct!")
+            
+        else:
+            
+            return render_template("riddles.html", username = username, data = data[riddle_index]["question"], correct = "Wrong answer, try again")
+            
+    elif request.method == "POST" and "next" in request.form:
+        riddle_index += 1
+        #if len(data) == riddle_index:
+            #move to new a page
+        
+        return render_template("riddles.html", username = username, data = data[riddle_index]["question"])
+    
+    elif request.method == "POST" and "previous" in request.form:
+        riddle_index -= 1
+        if len(data) < 0:
+            riddle_index = 0
+        return render_template("riddles.html", username = username, data = data[riddle_index]["question"])
+
+    
+    
+    return render_template("riddles.html", username = username, data = data[riddle_index]["question"])
+    
+    
     
 
 
+  
 app.run(os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
